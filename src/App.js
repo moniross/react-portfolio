357ff568e8e490513601fddfa5063349972179d5
moniross <monirfouzi20@gmail.com>
2022-05-30 14:33:22 +0000

    struction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./assets/scss/style.scss";
+import Nav from "./components/nav/Nav";
 import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Header />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
